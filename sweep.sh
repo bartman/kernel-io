a52613f327b7d68ad056285787df2444f87b1f95
Bart Trojanowski <bart@jukie.net>
2023-12-27 19:24:54 +0000

    sweep: delay between runs, run 10 times, fix num threads

--- sweep.sh
+++ sweep.sh
@@ -8,12 +8,12 @@ for th in 1 2 ; do
 	for ((qd=1; qd<255; qd*=2)) ; do
 		for rd in 0 1 25 33 50 66 75 99 100 ; do
 			for ((bu=1; bu<255; bu*=2)) ; do
-				for ((run=0; run<5; run++)) ; do
+				for ((run=0; run<10; run++)) ; do
 					# run once and discard, run again and capture
 					( set -e -x
 					./kio.py -c config.yaml \
 						--label run$run \
-						--num-threads 1 \
+						--num-threads $th \
 						--runtime 5 \
 						--burst-finish 1 \
 						--read-burst $bu \
@@ -24,6 +24,7 @@ for th in 1 2 ; do
 						--output-csv output.csv
 					)
 				done
+				sleep 30
 			done
 		done
 	done
